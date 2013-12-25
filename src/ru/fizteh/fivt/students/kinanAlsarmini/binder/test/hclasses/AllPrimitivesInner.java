37352db086986a090662b35a373e7989beaf823d
Sarkin <kinan.sarmini@gmail.com>
2013-12-26 01:06:46 +0300

    Update AllPrimitivesInner.java

--- src/ru/fizteh/fivt/students/kinanAlsarmini/binder/test/hclasses/AllPrimitivesInner.java
+++ src/ru/fizteh/fivt/students/kinanAlsarmini/binder/test/hclasses/AllPrimitivesInner.java
@@ -2,7 +2,7 @@ package ru.fizteh.fivt.students.kinanAlsarmini.binder.test.hclasses;
 
 public class AllPrimitivesInner {
     int a;
-    protected Long b;
+    protected long b;
     public String c;
     private Day d;
 
