5f3ed24c684817cd954843cc38465b3707d4341d
dmitrybaldin <dmitry.baldin@yandex.ru>
2013-12-26 02:35:01 +0400

    again

--- src/ru/fizteh/fivt/students/baldindima/junit/DataBase.java
+++ src/ru/fizteh/fivt/students/baldindima/junit/DataBase.java
@@ -221,7 +221,7 @@ public class DataBase implements Table, AutoCloseable {
         readLock.lock();
         try {
         	
-            if ((files.containsKey(nFileInMap)) && (files.get(nFileInMap).mapFromFile != null)) {
+            if ((files.containsKey(nFileInMap)) && (files.get(nFileInMap) != null) && (files.get(nFileInMap).mapFromFile != null)) {
                	result = files.get(nFileInMap).mapFromFile.get(keyString);
             } else {
                 try {
