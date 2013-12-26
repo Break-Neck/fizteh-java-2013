548f525203ff453fe43f1df3db027c89b3f36ad8
isItJavaOrSomething <nikrivchanskii@yandex.ru>
2013-12-26 11:49:08 +0400

    Maybe not

--- src/ru/fizteh/fivt/students/krivchansky/multifilemap/MultiFileMapShellState.java
+++ src/ru/fizteh/fivt/students/krivchansky/multifilemap/MultiFileMapShellState.java
@@ -3,17 +3,18 @@ import java.io.IOException;
 
 import ru.fizteh.fivt.students.krivchansky.filemap.FileMapShellState;
 import ru.fizteh.fivt.students.krivchansky.filemap.MyTable;
+import ru.fizteh.fivt.storage.strings.Table;
 import ru.fizteh.fivt.storage.strings.TableProvider;
 
 public class MultiFileMapShellState extends FileMapShellState implements MultifileMapShellStateInterface<MyTable, String, String> {
 	    public TableProvider tableProvider;
 
 		public MyTable useTable(String name) {
-			MyTable tempTable = (MyTable) tableProvider.getTable(name);
+			Table tempTable = tableProvider.getTable(name);
 			if (tempTable != null) {
-				table = tempTable;
+				table = (MyTable) tempTable;
 			}
-			return tempTable;
+			return (MyTable) tempTable;
 		}
 
 		public MyTable createTable(String args) {
