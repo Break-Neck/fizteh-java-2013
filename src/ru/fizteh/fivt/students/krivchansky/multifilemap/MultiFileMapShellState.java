ec94651b36986f74f9cfd48bdfd57af884e1e624
isItJavaOrSomething <nikrivchanskii@yandex.ru>
2013-12-26 12:12:33 +0400

    Revert "Maybe not"
    This reverts commit 548f525203ff453fe43f1df3db027c89b3f36ad8.

--- src/ru/fizteh/fivt/students/krivchansky/multifilemap/MultiFileMapShellState.java
+++ src/ru/fizteh/fivt/students/krivchansky/multifilemap/MultiFileMapShellState.java
@@ -3,18 +3,17 @@ import java.io.IOException;
 
 import ru.fizteh.fivt.students.krivchansky.filemap.FileMapShellState;
 import ru.fizteh.fivt.students.krivchansky.filemap.MyTable;
-import ru.fizteh.fivt.storage.strings.Table;
 import ru.fizteh.fivt.storage.strings.TableProvider;
 
 public class MultiFileMapShellState extends FileMapShellState implements MultifileMapShellStateInterface<MyTable, String, String> {
 	    public TableProvider tableProvider;
 
 		public MyTable useTable(String name) {
-			Table tempTable = tableProvider.getTable(name);
+			MyTable tempTable = (MyTable) tableProvider.getTable(name);
 			if (tempTable != null) {
-				table = (MyTable) tempTable;
+				table = tempTable;
 			}
-			return (MyTable) tempTable;
+			return tempTable;
 		}
 
 		public MyTable createTable(String args) {
