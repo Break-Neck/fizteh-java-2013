fac1e29ba81b70e43b280b0fb1c4a8507785a4ae
Sergey <melikov.sergey@gmail.com>
2013-12-26 11:25:34 +0400

    -

--- src/ru/fizteh/fivt/students/sergeymelikov/multifilemap/MyTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/sergeymelikov/multifilemap/MyTableProviderFactory.java
@@ -9,11 +9,15 @@ public class MyTableProviderFactory implements  TableProviderFactory {
     @Override
     public ExtendProvider create(String dataBaseDir) {
         if (dataBaseDir == null) {
-           throw new IllegalArgumentException("dir not defined");
+           //throw new IllegalArgumentException("dir not defined");
+           System.err.println("dir not defined");
+           System.exit(1);
         }
         File directory = new File(dataBaseDir);
         if (!directory.isDirectory()) {
-            throw new IllegalArgumentException(dataBaseDir + " is not a directory name");
+            //throw new IllegalArgumentException(dataBaseDir + " is not a directory name");
+            System.err.println(dataBaseDir + " is not a directory name");
+            System.exit(1);
         }
         return new MyTableProvider(directory);
     }
